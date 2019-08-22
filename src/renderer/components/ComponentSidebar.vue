32e6be181ca0241ce07b0e948dc59588face437a
FlorianWoelki <FlorianWoelki@gmx.de>
2019-08-22 14:51:58 +0200

    :sparkles: Added component sidebar

---
+++ src/renderer/components/ComponentSidebar.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-sidebar style="background: #2A2827 !important;">
+    <v-sidebar-items style="background #373535 !important;">
+      <v-sidebar-item></v-sidebar-item>
+      <v-sidebar-item link="Test">Test</v-sidebar-item>
+    </v-sidebar-items>
+    <v-sidebar-content>
+      <v-container>
+        <h4 class="white light">Pretty Dope stuff</h4>
+      </v-container>
+    </v-sidebar-content>
+  </v-sidebar>
+</template>
+
+<style lang="scss">
+#sidebar {
+  background: #373535 !important;
+  border-right: 1px solid #000;
+  
+  .tree-item-header {
+    color: white !important;
+  }
+}
+</style>
--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -1,16 +1,13 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col c12>
-        <h1 class="light">Hello World</h1>
-      </v-col>
-      <v-col c12>
-        <p>This is going to be a super funny UI builder project.</p>
-        <p>Let's see where it goes!</p>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-btn accent loadingLeft class="clickable">Click me</v-btn>
-    </v-row>
-  </v-container>
+  <ComponentSidebar />
 </template>
+
+<script>
+import ComponentSidebar from './ComponentSidebar.vue';
+
+export default {
+  components: {
+    ComponentSidebar,
+  },
+};
+</script>
